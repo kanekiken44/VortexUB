b57404c36cb5404e7ce870b6ef1171a590bb93f6
LoneGhoul <81549240+kanekiken44@users.noreply.github.com>
2022-02-07 13:33:30 +0530

    Update __init__.py

--- Vortex/plugins/sql_helper/__init__.py
+++ Vortex/plugins/sql_helper/__init__.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from Vortex.VortexbotConfig import Var
+from Vortex.Config import Var
 
 
 
