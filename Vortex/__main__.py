7afd47f198348fc018adca7c19675c9b8783a22c
LoneGhoul <81549240+kanekiken44@users.noreply.github.com>
2022-03-24 15:55:50 +0530

    Update __main__.py

--- Vortex/__main__.py
+++ Vortex/__main__.py
@@ -1,5 +1,5 @@
 import glob
-from Vortex import bot
+from Vortex import Vortex
 from sys import argv
 from telethon import TelegramClient
 import Vortex
@@ -16,34 +16,34 @@ LOAD_MYBOT = Var.LOAD_MYBOT
 logo = "VortexUB"
 
 async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me()
-    bot.uid = telethon.utils.get_peer_id(bot.me)
+    await Vortex.start(bot_token)
+    Vortex.me = await Vortex.get_me()
+    Vortex.uid = telethon.utils.get_peer_id(Vortex.me)
 
 
 async def startup_log_all_done():
     try:
-        await bot.send_file(START_GRP, PIC, caption='**Vortex UserBot has been started**')
+        await Vortex.send_file(START_GRP, PIC, caption='**Vortex UserBot has been started**')
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
 if len(argv) not in (1, 3, 4):
-    bot.disconnect()
+    Vortex.disconnect()
 else:
-    bot.tgbot = None
+    Vortex.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         print("Initiating Inline Bot")
-        bot.tgbot = TelegramClient(
+        Vortex.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
         print("Starting Userbot")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        Vortex.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Startup Completed")
     else:
-        bot.start()
+        Vortex.start()
 
 path = 'Vortex/plugins/*.py'
 files = glob.glob(path)
@@ -55,10 +55,10 @@ for name in files:
 
 print("Vortex Userbot has been deployed! ")
 
-bot.loop.run_until_complete(startup_log_all_done())
+Vortex.loop.run_until_complete(startup_log_all_done())
 
 if len(argv) in {1, 3, 4}:
-    bot.run_until_disconnected()
+    Vortex.run_until_disconnected()
 
 else:
-    bot.disconnect()
+    Vortex.disconnect()
